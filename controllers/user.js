20b2fda88a434f09377899deb6d89c2b15b1e14e
CRS <crs@His-Name-is-Jerry-Maxey.local>
2017-10-11 14:00:04 -0700

    removed more html

--- controllers/user.js
+++ controllers/user.js
@@ -281,8 +281,8 @@ exports.postReset = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Your Hackathon Starter password has been changed',
+      from: 'john@crs-consulting.com',
+      subject: 'Your Password has been changed',
       text: `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`
     };
     return transporter.sendMail(mailOptions)
@@ -355,8 +355,8 @@ exports.postForgot = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Reset your password on Hackathon Starter',
+      from: 'john@crs-consulting.com',
+      subject: 'Reset your password on NTW Project',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
